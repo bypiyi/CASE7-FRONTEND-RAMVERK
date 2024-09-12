5a336ae59f77034537b43e868f344a6d5fe152d7
Alicia <23alicia.piyi@glimnet.se>
2024-09-12 10:13:29 +0300

    Fixed moviecards. Still working on design

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -14,13 +14,26 @@ function Movies() {
 
   return (
     <section id="movies" className="movies-section">
-      <h2>Movies</h2>
       <div className="movies-grid">
         {movies.length > 0 ? (
           movies.map(movie => (
             <div key={movie.id} className="movie-card">
               <h3>{movie.title}</h3>
               <p>{movie.description}</p>
+              <p><b>Genre:</b> {movie.genre}</p>
+              {/* <p>{movie.releaseDate}</p> */}
+              <p><b>Director:</b> {movie.director}</p>
+              <p><b>Duration min:</b> {movie.duration}</p>
+              {/* <p>{movie.rating}</p> */}
+              <img src={movie.posterUrl} alt={`${movie.title} Poster`} className="movie-poster" />
+
+              {/* buttons */}
+              <div className="button-group">
+                <button className="btn btn-trailer">TRAILER</button>
+                <a href={`https://cinema-api.henrybergstrom.com/#/Booking/book_tickets/${movie.id}`} className="btn btn-book">Book tickets</a>
+              </div>
+
+
             </div>
           ))
         ) : (
